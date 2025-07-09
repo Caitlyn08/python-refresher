828455011aa95fad242ea83d8ed0ce8d912f702e
caitlyn <caitlynbarton08@gmail.com>
2025-07-09 15:07:01 -0400

    change

--- test_account.py
+++ test_account.py
@@ -10,13 +10,11 @@ class TestAccount(unittest.TestCase):
         self.setUp()
         self.assertEqual(self.acc.withdraw(5), 5)
         self.assertRaises(ValueError, self.acc.withdraw, 10)
-        self.assertEqual(self.acc.current_balance(), 5)
 
     def test_deposit(self):
         self.setUp()
         self.assertEqual(self.acc.deposit(5), 15)
         self.assertNotEqual(self.acc.deposit(20), 40)
-        self.assertEqual(self.acc.current_balance(), 15)
 
     def test_current_balance(self):
         self.setUp()
