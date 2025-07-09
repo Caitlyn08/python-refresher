fd89845609b20c6485ebbaa4816c7f437f34366f
caitlyn <caitlynbarton08@gmail.com>
2025-07-09 16:54:22 -0400

    valueerror

--- account.py
+++ account.py
@@ -1,16 +1,18 @@
 class Account:
-    def __init__(self, name, account_number, balance=0):
+    def __init__(self, name, account_number, balance):
         self.name = name
         self.account_number = account_number
         self.balance = balance
 
     def withdraw(self, number):
+        if number > self.balance or number < 0:
+            raise ValueError("not valid")
         self.balance -= number
-        if number > self.balance:
-            raise ValueError("Not enough")
         return self.balance
 
     def deposit(self, number):
+        if number < 0:
+            raise ValueError("not valid")
         self.balance += number
         return self.balance
 
