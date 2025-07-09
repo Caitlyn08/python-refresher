d475f898815bf5e961ceaa3ff8d22d883d9fa70d
caitlyn <caitlynbarton08@gmail.com>
2025-07-09 11:47:57 -0400

    Initial commit

--- test_hello.py
+++ test_hello.py
@@ -5,6 +5,8 @@ import hello
 class TestHello(unittest.TestCase):
     def test_hello(self):
         self.assertEqual(hello.hello(), "Hello, world!")
+        self.assertNotEqual(hello.hello(), "bye, world!")
+        self.assertTrue(isinstance(hello.hello(), str))
 
     def test_sin(self):
         self.assertEqual(hello.sin(0), 0)
@@ -21,7 +23,22 @@ class TestHello(unittest.TestCase):
     def test_cot(self):
         self.assertEqual(hello.cot(0), float("inf"))
         self.assertEqual(hello.cot(1), 0.6420926159343306)
+    
+    def test_add(self):
+        self.assertEqual(hello.add(2,3), 5)
+        self.assertNotEqual(hello.add(2,5), 10)
 
+    def test_sub(self):
+        self.assertEqual(hello.sub(5,3), 2)
+        self.assertNotEqual(hello.sub(5,3), 1)
+
+    def test_mul(self):
+        self.assertEqual(hello.mul(2,3), 6)
+        self.assertNotEqual(hello.mul(2,3), 5)
+
+    def test_div(self):
+        self.assertEqual(hello.div(6,3), 2)
+        self.assertRaises(ValueError, hello.div, 6, 0)
 
 if __name__ == "__main__":
     unittest.main()
