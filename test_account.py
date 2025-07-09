044e378f7f6ba521e1f561cf5087f7284f3f9a32
caitlyn <caitlynbarton08@gmail.com>
2025-07-09 15:05:35 -0400

    change

--- test_account.py
+++ test_account.py
@@ -2,23 +2,23 @@ import unittest
 import account
 
 
-class TestAccount(unittest.Testcase):
-    def set(self):
+class TestAccount(unittest.TestCase):
+    def setUp(self):
         self.acc = account.Account("Caitlyn", "222", 10)
 
     def test_withdraw(self):
-        self.set()
+        self.setUp()
         self.assertEqual(self.acc.withdraw(5), 5)
         self.assertRaises(ValueError, self.acc.withdraw, 10)
         self.assertEqual(self.acc.current_balance(), 5)
 
     def test_deposit(self):
-        self.set()
+        self.setUp()
         self.assertEqual(self.acc.deposit(5), 15)
         self.assertNotEqual(self.acc.deposit(20), 40)
         self.assertEqual(self.acc.current_balance(), 15)
 
     def test_current_balance(self):
-        self.set()
+        self.setUp()
         self.assertEqual(self.acc.current_balance(), 10)
         self.assertNotEqual(self.acc.current_balance(), 5)
