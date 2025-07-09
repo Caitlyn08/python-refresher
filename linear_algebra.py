0381e773f9eebbccaf885b523dfc124bdbe76045
caitlyn <caitlynbarton08@gmail.com>
2025-07-09 16:55:10 -0400

    alg

--- linear_algebra.py
+++ linear_algebra.py
@@ -1,15 +1,21 @@
 import numpy as np
 
+
 def add_matrices(a, b):
     return np.add(a, b)
+
 
 def subtract_matrices(a, b):
     return np.subtract(a, b)
 
-def magnitude(a):
-    return np.linalg.norm(a)
 
 def dot_product(a, b):
     return np.dot(a, b)
 
-def 
+
+def magnitude(a):
+    return np.linalg.norm(a)
+
+
+def transpose_matrix(a):
+    return a.T
