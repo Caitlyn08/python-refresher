a37206d096695e8f5c2699b1a2df65feec3f090c
caitlyn <caitlynbarton08@gmail.com>
2025-07-10 16:33:01 -0400

    a

--- test_physics.py
+++ test_physics.py
@@ -2,18 +2,19 @@ import unittest
 import physics
 import numpy as np
 
+
 class testPhysics(unittest.TestCase):
-    mass=100
-    volume=0.1
+    mass = 100
+    volume = 0.1
 
     def test_calculate_buoyancy(self):
-        self.assertEqual(physics.calculate_buoyancy(1,3), 29.43)
-    
+        self.assertEqual(physics.calculate_buoyancy(1, 3), 29.43)
+
     def test_will_it_float(self):
         self.assertEqual(physics.will_it_float(self.volume, self.mass), False)
 
     def test_calculate_pressure(self):
-        self.assertEqual(physics.calculate_pressure(20),196200.00000000003)
+        self.assertEqual(physics.calculate_pressure(20), 196200.00000000003)
 
     def test_calculate_acceleration(self):
-        
+        self.assertEqual(physics.calculate_acceleration(20, 5), 4)
